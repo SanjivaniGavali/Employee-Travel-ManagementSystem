03ac8b804a7fbe6250305125b7836264c4b629cf
Sanjivani <sanjivanigavali999@gmail.com>
2023-11-01 00:24:29 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UI_Class_Library;
+namespace TravelBookingProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu m1 = new Menu();
+            m1.MainMenu();
+            Console.ReadLine();
+            //gghfgghv
+
+        }
+    }
+}
